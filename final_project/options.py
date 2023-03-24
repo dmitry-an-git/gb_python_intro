90b81dc3af7085eb7773812ff8346b0114b69d2f
Dmitry An <dmitry.andreyev@gmail.com>
2023-03-24 17:52:09 +0400

    final commit

--- final_project/options.py
+++ final_project/options.py
@@ -11,13 +11,3 @@ options = [
     "Quit"
 ]
 
-options_dic = {
-    "Load phonebook from file":phone_book.load_from_file(),
-    "Print phonebook":print(phone_book),
-    "Add new contact":phone_book.add_entry(),
-    "Find contact":phone_book.search_entries(),
-    "Edit contact":phone_book.edit_entry(),
-    "Remove contact":phone_book.delete_entry(),
-    "Save phonebook to file":phone_book.save_to_file(),
-    "Quit": StopIteration
-}
